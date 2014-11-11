376c48d2a4506710c2dd684458119b23958c1946
Ed Ball <info@aptary.com>
2014-11-11 14:22:48 +0000

    Add some more fun n games to home page

--- js/home.js
+++ js/home.js
@@ -24,3 +24,55 @@ $(".js-pipe").click(function(){
 		$(this).data("up", false);
 	}
 });
+
+var colors = ["#00C4B3", "#F7323F", "#F9BE00"];
+$(".js-arc").click(function(){
+	colors.push(colors.shift());
+	$(".js-arc path").each(function(i){
+		$(this).attr("fill", colors[i]);
+	})
+});
+
+
+
+$(document).ready(function(){
+
+	function Ball(translateX, translateY, rotation){
+		this.translateX = -180;
+		this.translateX = translateX;
+		this.translateY = translateY;
+		this.rotation = rotation;
+		this.gravity = 0.075;
+		this.vy = 0;
+	}
+
+	balls = [];
+
+	$(".js-circle").each(function(i){
+		balls.push(new Ball(-180 + i * 175, 0, i*60))
+	});
+
+	function animate(){
+		window.requestAnimationFrame(animate);
+
+		for (var i = 0 ; i < balls.length ; i++){
+			balls[i].translateX += 1.5;
+			balls[i].rotation += 2;
+			if (balls[i].translateX > 220){
+				balls[i].vy+=balls[i].gravity;
+				balls[i].translateY+=balls[i].vy;
+			}
+			if (balls[i].translateY > 310){
+				balls[i].translateX = -180;
+				balls[i].translateY = 0;
+				balls[i].vy = 0;
+			}
+
+			$(".js-circle").eq(i).css("transform", "translateX("+balls[i].translateX+"px) translateY("+balls[i].translateY+"px) rotate("+balls[i].rotation+"deg)");
+
+		}
+
+	}
+
+	animate();
+})
