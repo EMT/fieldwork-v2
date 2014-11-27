1f5016b747e938781296a61fc6d8973c2a3881b8
Ed Ball <info@aptary.com>
2014-11-27 15:53:49 +0000

    Latest updates to map

--- js/map.js
+++ js/map.js
@@ -1,158 +1,135 @@
-$(function(){
-	var map;
-	var markers = [];
-	var locations = [
-		{
-			title: "",
-			desc: "Planet Fieldwork",
-			lat: 53.482035,
-			lng: -2.234324,
-			type: "fieldwork"
-		},
-		{
-			title: "Soup Kitchen",
-			desc: "Soup, soup, a tasty soup, soup.",
-			lat: 53.482752,
-			lng: -2.234573,
-			type: "eating"
-		},
-		// {
-		// 	title: "",
-		// 	desc: "Mozza's",
-		// 	lat: 53.481548, 
-		// 	lng: -2.236630
-		// },
-		{
-			title: "",
-			desc: "Pie 'n' mash",
-			lat: 53.482845,
-			lng: -2.236148,
-			type: "eating"
-		},
-		{
-			title: "",
-			desc: "Chip barms",
-			lat: 53.484083,
-			lng: -2.234382,
-			type: "eating"
-		},
-		{
-			title: "",
-			desc: "Drinks on the Terrace",
-			lat: 53.484181,
-			lng: -2.236759,
-			type: "drinking"
-		},
-		{
-			title: "",
-			desc: "Kosmonaut",
-			lat: 53.481226,
-			lng: -2.232364,
-			type: "drinking"
-		},
-		{
-			title: "",
-			desc: "Posh Sandwiches",
-			lat: 53.480078,
-			lng: -2.238798,
-			type: "eating"
-		},
-		{	
-			title: "",
-			desc: "South of Little Italy",
-			lat: 53.483575,
-			lng: -2.237054,
-			type: "eating"
-		},
-		{	
-			title: "",
-			desc: "Burgers 'n' gherkins",
-			lat: 53.480581,
-			lng: -2.248030,
-			type: "eating"
-		}
-	];
+// Provide your access token
+L.mapbox.accessToken = 'pk.eyJ1IjoiZWRmaWVsZHdvcmsiLCJhIjoicTMwZTkydyJ9.PReYJSCAJ4cFTBOsINez8A';
 
-	// Google Map
-	function initializeMap() {
-		var latlng = new google.maps.LatLng(53.482035,-2.234324);
-		var settings = {
-			zoom: 16,
-			center: latlng,
-			scrollwheel: true,
-			navigationControl: false,
-			scaleControl: false,
-			streetViewControl: false,
-			draggable: true,
-			scrollwheel: false, 
-			mapTypeControl: false,
-			mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-			navigationControl: false,
-			navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			zoomControlOptions: {
-			  style: google.maps.ZoomControlStyle.LARGE,
-			  position: google.maps.ControlPosition.LEFT_BOTTOM
-			},
-			panControl: false
+var map = L.mapbox.map('map', 'edfieldwork.kb21lg3g', { zoomControl: false })
+  .setView([53.482, -2.235], 16);
 
-		};
+new L.Control.Zoom({ position: 'bottomleft' }).addTo(map);
 
-		map = new google.maps.Map(document.getElementById('js-map'), settings);
 
-		
-		// var marker = new google.maps.Marker({
-		//   // icon: image,
-		//   map: map,
-		//   position: latlng,
-		//   title: "Planet Fieldwork"
-		// });
-		
-		
-		var map_styles = [
-		  {
-		    featureType: "all",
-		    stylers: [
-		      { saturation: -100 }
-		    ]
-		  }
-		];
-		
-		map.setOptions({styles: map_styles});
-		
+var locations = [
+	{
+		title: "",
+		desc: "Planet Fieldwork",
+		lat: 53.482035,
+		lng: -2.234324,
+		type: "fieldwork"
+	},
+	{
+		title: "Soup Kitchen",
+		desc: "Soup, soup, a tasty soup, soup.",
+		lat: 53.482752,
+		lng: -2.234573,
+		type: "eating"
+	},
+	// {
+	// 	title: "",
+	// 	desc: "Mozza's",
+	// 	lat: 53.481548, 
+	// 	lng: -2.236630
+	// },
+	{
+		title: "",
+		desc: "Pie 'n' mash",
+		lat: 53.482845,
+		lng: -2.236148,
+		type: "eating"
+	},
+	{
+		title: "",
+		desc: "Chip barms",
+		lat: 53.484083,
+		lng: -2.234382,
+		type: "eating"
+	},
+	{
+		title: "",
+		desc: "Drinks on the Terrace",
+		lat: 53.484181,
+		lng: -2.236759,
+		type: "drinking"
+	},
+	{
+		title: "",
+		desc: "Kosmonaut",
+		lat: 53.481226,
+		lng: -2.232364,
+		type: "drinking"
+	},
+	{
+		title: "",
+		desc: "Posh Sandwiches",
+		lat: 53.480078,
+		lng: -2.238798,
+		type: "eating"
+	},
+	{	
+		title: "",
+		desc: "South of Little Italy",
+		lat: 53.483575,
+		lng: -2.237054,
+		type: "eating"
+	},
+	{	
+		title: "",
+		desc: "Burgers 'n' gherkins",
+		lat: 53.480581,
+		lng: -2.248030,
+		type: "eating"
 	}
+];
 
-	function setupListener(infoWindow,marker){
-		google.maps.event.addListener(marker, 'click', function() {
-		  infoWindow.open(map,marker);
-		});
-	}
+var myLayer = L.mapbox.featureLayer().addTo(map);
+var geoJson = [];
+
+for (var i = 0 ; i < locations.length ; i++){
+
+	geoJson.push({
+	    type: 'Feature',
+	    geometry: {
+	        type: 'Point',
+	        coordinates: [locations[i].lng, locations[i].lat]
+	    },
+	    content: locations[i].desc,
+	    properties: {
+	    	'title': locations[i].desc,
+            'marker-color': i == 0 ? '#e83944' : '#00c4b3'
+	    }
+	});
+
+}
 
 
-	initializeMap();
+myLayer.setGeoJSON({
+  features: geoJson
+});
+myLayer.on('click', function(e) {
+      map.panTo(e.layer.getLatLng());
+      $(".js-map-info").html()
+  });
 
-	for (var i = 0 ; i < locations.length ; i++){
-		if (locations[i].type == "drinking"){
-			imagePath = "/fieldwork-v2/images/map/marker-orange.svg";
-		} else if (locations[i].type == "eating"){
-			imagePath = "/fieldwork-v2/images/map/marker-turquoise.svg";
-		} else {
-			imagePath = "/fieldwork-v2/images/map/marker-red.svg";
-		}
-		var image = new google.maps.MarkerImage(imagePath);
+// myLayer.eachLayer(function(marker) {
+// 	marker.on('click', function(e){
+// 		$(".js-map-info h2").html(marker.feature.content);
+// 	})
+// })
 
-		markers.push(new google.maps.Marker({
-			icon: image,
-			map: map,
-			position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
-			title: locations[i].desc
-		}));
+map.scrollWheelZoom.disable();
 
-		var infoWindow = new google.maps.InfoWindow({
-		     content: locations[i].desc
-		 });
+// function resetColors() {
+//     for (var i = 0; i < geoJson.length; i++) {
+//         geoJson[i].properties['marker-color'] = geoJson[i].properties['old-color'] ||
+//             geoJson[i].properties['marker-color'];
+//     }
+//     // myLayer.setGeoJSON(geoJson);
+// }
 
-		setupListener(infoWindow,markers[i])
+// myLayer.on('click', function(e) {
+//     resetColors();
+//     e.layer.feature.properties['old-color'] = e.layer.feature.properties['marker-color'];
+//     e.layer.feature.properties['marker-color'] = '#ff8888';
+//     myLayer.setGeoJSON(geoJson);
+// });
 
-	}
-})
+// map.on('click', resetColors);
+
